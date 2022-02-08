41da7e82061df63f34e507e50c2febffa60b4570
ntdat89.hn@gmail.com <Dat29489@>
2022-02-08 10:35:16 +0700

    .

---
+++ src/components/Charts/DonutChart.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Pie } from '@ant-design/charts'
+
+const DonutChart = props => {
+  const data = [
+    {
+      type: 'Top Up',
+      value: 27,
+    },
+    {
+      type: 'SMS',
+      value: 25,
+    },
+    {
+      type: 'BrandName',
+      value: 18,
+    },
+    {
+      type: 'Data',
+      value: 15,
+    },
+    {
+      type: 'Call center',
+      value: 10,
+    },
+    {
+      type: 'Vas',
+      value: 5,
+    },
+  ]
+  const config = {
+    data,
+    appendPadding: 10,
+    angleField: 'value',
+    colorField: 'type',
+    radius: 1,
+    innerRadius: 0.6,
+    label: {
+      type: 'inner',
+      offset: '-50%',
+      content: '{value}',
+      style: {
+        textAlign: 'center',
+        fontSize: 14,
+      },
+    },
+    interactions: [
+      {
+        type: 'element-selected',
+      },
+      {
+        type: 'element-active',
+      },
+    ],
+    statistic: {
+      title: false,
+      content: {
+        style: {
+          whiteSpace: 'pre-wrap',
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+        },
+        content: 'Iris',
+      },
+    },
+    legend: {
+      // title: '',
+      position: 'bottom',
+    },
+  }
+  return <Pie {...config} />
+}
+
+DonutChart.propTypes = {}
+
+export default DonutChart
--- src/pages/WebApp/TestPage/TestPage.js
+++ src/pages/WebApp/TestPage/TestPage.js
@@ -5,84 +5,12 @@ import { TestPageWrapper } from './TestPageStyled'
 import UploadCropModule from '../../../components/UploadCropModule'
 import DefaultLayout from '../../../layouts/DefaultLayout/DefaultLayout'
 import { Helmet } from 'react-helmet/es/Helmet'
-import { Line } from '@ant-design/plots';
+import LineChart from '../../../components/Charts/LineChart'
+import ColumnChart from '../../../components/Charts/ColumnChart'
+import BarChart from '../../../components/Charts/BarChart'
+import DonutChart from '../../../components/Charts/DonutChart'
 
 
-const DemoLine = () => {
-  const data = [
-    {
-      year: '1991',
-      value: 3,
-    },
-    {
-      year: '1992',
-      value: 4,
-    },
-    {
-      year: '1993',
-      value: 3.5,
-    },
-    {
-      year: '1994',
-      value: 5,
-    },
-    {
-      year: '1995',
-      value: 4.9,
-    },
-    {
-      year: '1996',
-      value: 6,
-    },
-    {
-      year: '1997',
-      value: 7,
-    },
-    {
-      year: '1998',
-      value: 9,
-    },
-    {
-      year: '1999',
-      value: 13,
-    },
-  ];
-  const config = {
-    data,
-    xField: 'year',
-    yField: 'value',
-    label: {
-
-    },
-    point: {
-      size: 5,
-      shape: 'diamond',
-      style: {
-        fill: 'white',
-        stroke: '#5B8FF9',
-        lineWidth: 2,
-      },
-    },
-    tooltip: {
-      showMarkers: false,
-    },
-    state: {
-      active: {
-        style: {
-          shadowBlur: 4,
-          stroke: '#000',
-          fill: 'red',
-        },
-      },
-    },
-    interactions: [
-      {
-        type: 'marker-active',
-      },
-    ],
-  };
-  return <Line {...config} />;
-};
 
 const TestPageStyled = props => {
   const { commonStore } = props
@@ -102,9 +30,13 @@ const TestPageStyled = props => {
         <title>Test</title>
       </Helmet>
       <TestPageWrapper>
-        <UploadCropModule callbackFileCropped={(e) => setFileToUpload(e)} callbackFileSrcPreview={(e) => setFileToPreview(e)} grid={true} rotate={true} />
+        <UploadCropModule callbackFileCropped={(e) => setFileToUpload(e)}
+                          callbackFileSrcPreview={(e) => setFileToPreview(e)} grid={true} rotate={true} />
         <img src={fileToPreview} />
-        <DemoLine />
+        <LineChart />
+        <ColumnChart />
+        <BarChart />
+        <DonutChart />
       </TestPageWrapper>
     </DefaultLayout>
 
